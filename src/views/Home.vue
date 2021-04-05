ae0b334c081ef6ede3ae19b7025f6b1fdb067fd6
JessicaHoang <jessica97hoang@gmail.com>
2021-04-04 19:12:23 -0700

    components: header and footer
    added header and footer components. Footer is not yet functional, and have not been added to Home, but Header is ok.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- header component -->
+    <header1 />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footer />
+    
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
+// components of the home page imported
 import HelloWorld from '@/components/HelloWorld.vue'
+import header1 from '@/components/header1'
+import footer from '@/components/footer'
 
+// components of the home page exported
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    header1,
+    footer
   }
 }
 </script>
+
+<style scoped lang="scss">
+#header1 {
+  text-align: left;
+}
+</style>
